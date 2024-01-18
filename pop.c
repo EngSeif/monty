0f4e6e610ed1830d67f3d21c1c7e2823f336da91
EngSeif <eng.seifmohamed2004@gmail.com>
2024-01-18 18:47:27 +0200

    Mandatory C Monty | fix pop

--- pop.c
+++ pop.c
@@ -12,7 +12,7 @@ void pop(stack_t **stack, unsigned int line_number)
 
 	if (arguments->head == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		freeAll();
 		exit(EXIT_FAILURE);
 	}
